133c387cfc3e659748f5adcb36744a3370420cd7
root <root@DESKTOP-OAQH2K3>
2024-07-15 14:59:24 +0900

    コミット

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-money = 3000
-if money >=2000:
-    print ('映画間に行きます')
+freetime = True
+AM = True
+PM = True
+
+yuuki = [freetime, AM, PM]
+
+if yuuki[0] and yuuki[1]:
+    print("かえります")
+elif yuuki[0] and yuuki[2]:
+    print("かえりません")
+else:
+    print("寝ます")
